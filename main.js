9d0ff44d8c24511933f8e64b3669de2b46e5d314
Peter Jackson Link III <pjacksonlink@gmail.com>
2020-09-17 17:30:41 -0400

    Basic App

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow, BrowserView } = require('electron')
+
+
+const width = 1000
+const height = 600
+
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: width,
+        height: height,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: false,
+            frame: false
+        }
+    })
+
+    win.setMenu(null)
+
+    const view = new BrowserView()
+
+    win.setBrowserView(view)
+    view.setBounds({ x: 0, y: 0, width: width, height: height})
+    view.setAutoResize({ width: true, height: true, horizontal: true, vertical: true})
+    view.webContents.loadURL('https://flashcardsmobile.com')
+}
+
+app.whenReady().then(createWindow)
